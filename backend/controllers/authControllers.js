e7cf2f4eb66a5458102817cff2421eb709b3b5ba
Binay <binaydas1123@gmail.com>
2024-09-21 20:27:57 +0530

    Code organisastion done for backend files

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const signUp = async (req, res) => {
     const { name, email, password } = req.body;
+    
     if (!name || !email || !password) {
         return res.status(400).json({ message: 'Please provide all fields' });
     }
