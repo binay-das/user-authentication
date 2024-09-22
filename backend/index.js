cfd87ac364d401aac92113c43798c20c26b77137
Binay <binaydas1123@gmail.com>
2024-09-22 23:02:03 +0530

    Managed routes

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const jwt = require('jsonwebtoken');
 const connectToDB = require('./connect');
-const User = require('./models/userModel');
-const bcrypt = require('bcrypt');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
 
 app.use(express.json());
+
 app.use(cors(
     {
         origin: 'http://localhost:5173',
@@ -25,32 +23,12 @@ connectToDB()
     .catch((err) => console.log(err));
 
 
-
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/api/login', (req, res) => {
-    res.json({ message: 'Login successful' });
-});
-
-app.get('/api/signup', (req, res) => {
-    res.json({ message: 'Signup successful' });
-});
-
 app.use('/api', authRoutes);
 
-app.post('/api/logout', (req, res) => {
-
-    res.clearCookie('token', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-    });
-
-    res.status(200).send({ message: 'Logged out successfully' });
-    
-})
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
